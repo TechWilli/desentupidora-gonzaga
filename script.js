bf534103746e90146d55feff44feec77497f8b59
William Araujo <williamf1f2@gmail.com>
2022-03-08 22:13:28 -0300

    Adiciona mais informações sobre o serviçõ. Cria modal para aparecer ao clicar nos botões de ligação ou mensagem com lista de números para acesso

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const backToTopButton = document.querySelector('.scroll-to-top-button');
 const whatsappHref = document.querySelectorAll('.whatsapp-send-message');
+const whatsappHrefDDD011 = document.querySelector('.whatsapp-send-message-ddd-11');
+const whatsappHrefDDD013 = document.querySelector('.whatsapp-send-message-ddd-13');
+const whatsappHrefDDD019 = document.querySelector('.whatsapp-send-message-ddd-19');
+
+const callModal = document.querySelector('.call-modal-ddd-choice');
+const messageModal = document.querySelector('.message-modal-ddd-choice');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+const closeModal = document.querySelectorAll('.close-modal');
 
 function addWhatsAppHref() {
   // console.log('whatsappHref', whatsappHref)
   const messageToSend = 'Olá, vim pelo site site_aqui. Preciso de mais informações sobre!';
-  whatsappHref[0].href = `https://api.whatsapp.com/send?phone=numero_aqui&text=${encodeURI(messageToSend)}`;
-  whatsappHref[1].href = `https://api.whatsapp.com/send?phone=numero_aqui&text=${encodeURI(messageToSend)}`;
+  // whatsappHref[0].href = `https://api.whatsapp.com/send?phone=numero_aqui&text=${encodeURI(messageToSend)}`;
+  // whatsappHref[1].href = `https://api.whatsapp.com/send?phone=numero_aqui&text=${encodeURI(messageToSend)}`;
+
+  whatsappHrefDDD011.href = `https://api.whatsapp.com/send?phone=numero_aqui011&text=${encodeURI(messageToSend)}`;
+  whatsappHrefDDD013.href = `https://api.whatsapp.com/send?phone=numero_aqui013&text=${encodeURI(messageToSend)}`;
+  whatsappHrefDDD019.href = `https://api.whatsapp.com/send?phone=numero_aqui019&text=${encodeURI(messageToSend)}`;
 }
 
-function handleWhatsappClick() {
-  whatsappHref[1].click();
+function handleShowMessageModal() {
+  document.body.style.overflow = 'hidden';
+
+  messageModal.classList.toggle('show-modal');
+  modalOverlay.classList.toggle('show-modal');
+
+}
+
+function handleShowCallModal() {
+  document.body.style.overflow = 'hidden';
+
+  callModal.classList.toggle('show-modal');
+  modalOverlay.classList.toggle('show-modal');
 }
 
 function goToTop() {
@@ -19,7 +43,18 @@ function goToTop() {
   });
 }
 
+function handleCloseModal() {
+  callModal.classList.remove('show-modal');
+  messageModal.classList.remove('show-modal');
+  modalOverlay.classList.remove('show-modal');
+
+  document.body.style.overflow = 'auto';
+}
+
 backToTopButton.addEventListener('click', goToTop);
+
+closeModal[0].addEventListener('click', handleCloseModal);
+closeModal[1].addEventListener('click', handleCloseModal);
 
 window.onload = () => {
   addWhatsAppHref();
